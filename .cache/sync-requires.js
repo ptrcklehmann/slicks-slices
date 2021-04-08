9fb322bcec91b1ea536432311841014b14dd8d32
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-04-08 14:26:58 +0200

    add ContentStyles to layout component

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/patricklehmann/Documents/GitHub/slicks-slices/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/patricklehmann/Documents/GitHub/slicks-slices/src/pages/404.js"))),
   "component---src-pages-beers-js": hot(preferDefault(require("/Users/patricklehmann/Documents/GitHub/slicks-slices/src/pages/beers.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/patricklehmann/Documents/GitHub/slicks-slices/src/pages/index.js"))),
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
+import React from 'react'
+import styled from 'styled-components'
 import 'normalize.css'
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
 import GlobalStyles from '../styles/GlobalStyles'
-import Typography from '../styles/Typography';
+import Typography from '../styles/Typography'
+import stripes from '../assets/images/stripes.svg'
+
+
+const SiteBorderStyles = styled.div`
+    max-width: 1000px;
+    margin: 12rem auto 4rem auto;
+    margin-top: clamp(2rem, 10vw, 12rem);
+    background: white url(${stripes});
+    padding: 5px;
+    padding: clamp(5px, 1vw, 25px);
+    box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
+`
+
+const ContentStyles = styled.div`
+    background: white;
+    padding: 2rem;
+`
 
 
 export default function Layout({ children }) {
     return (
-        <div>
+        <>
             <GlobalStyles />
             <Typography />
-            <Nav />
-            {children}
-            <Footer />
-        </div>
-    );
+            <SiteBorderStyles>
+                <ContentStyles>
+                    <Nav />
+                    {children}
+                    <Footer />
+                </ContentStyles>
+            </SiteBorderStyles>
+        </>
+    )
 }
