a05103052e3de5b1ff6b11028b41a5bdd87cb6d9
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-04-09 17:21:20 +0200

    fix layout bug

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import styled from 'styled-components'
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
 import Logo from './Logo';
 
 const NavStyles = styled.nav`
-  margin-bottom: 3rem;
-
+  /* margin-bottom: 3rem; */
   .logo {
     transform: translateY(-25%);
   }
@@ -75,27 +74,27 @@ const NavStyles = styled.nav`
 `;
 
 export default function Nav() {
-    return (
-        <NavStyles>
-            <ul>
-                <li>
-                    <Link to="/">Hot Now</Link>
-                </li>
-                <li>
-                    <Link to="/pizzas">Pizzas</Link>
-                </li>
-                <li>
-                    <Link to="/">
-                    <Logo />
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/slicemasters">Slicemasters</Link>
-                </li>
-                <li>
-                    <Link to="/order">Order Ahead!</Link>
-                </li>
-            </ul>
-        </NavStyles>
-    )
+  return (
+    <NavStyles>
+      <ul>
+        <li>
+          <Link to="/">Hot Now</Link>
+        </li>
+        <li>
+          <Link to="/pizzas/">Pizza Menu</Link>
+        </li>
+        <li className="logo-item">
+          <Link to="/">
+            <Logo />
+          </Link>
+        </li>
+        <li>
+          <Link to="/slicemasters">SliceMasters</Link>
+        </li>
+        <li>
+          <Link to="/order">Order Ahead!</Link>
+        </li>
+      </ul>
+    </NavStyles>
+  );
 }
