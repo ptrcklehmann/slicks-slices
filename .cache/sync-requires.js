26c37796672d7540601b1df8f46e88d797abbc49
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-04-12 13:52:27 +0200

    add SinglePizzaPage and pizza template

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -11,6 +11,7 @@ exports.components = {
   "component---src-pages-index-js": hot(preferDefault(require("/Users/patricklehmann/Documents/GitHub/pizza-slicks-slices/slicks-slices/src/pages/index.js"))),
   "component---src-pages-order-js": hot(preferDefault(require("/Users/patricklehmann/Documents/GitHub/pizza-slicks-slices/slicks-slices/src/pages/order.js"))),
   "component---src-pages-pizzas-js": hot(preferDefault(require("/Users/patricklehmann/Documents/GitHub/pizza-slicks-slices/slicks-slices/src/pages/pizzas.js"))),
-  "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/patricklehmann/Documents/GitHub/pizza-slicks-slices/slicks-slices/src/pages/slicemasters.js")))
+  "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/patricklehmann/Documents/GitHub/pizza-slicks-slices/slicks-slices/src/pages/slicemasters.js"))),
+  "component---src-templates-pizza-js": hot(preferDefault(require("/Users/patricklehmann/Documents/GitHub/pizza-slicks-slices/slicks-slices/src/templates/Pizza.js")))
 }
 
--- src/components/ToppingsFilter.js
+++ src/components/ToppingsFilter.js
@@ -7,7 +7,7 @@ const ToppingsStyles = styled.div`
   flex-wrap: wrap;
   gap: 1rem;
   margin-bottom: 4rem;
-  a {
+  .topping-filter {
     display: grid;
     grid-template-columns: auto 1fr;
     grid-gap: 0 1rem;
@@ -36,7 +36,7 @@ function countPizzasInToppings(pizzas) {
       if (existingTopping) {
         console.log('Existing Topping', existingTopping.name);
         //  if it is, increment by 1
-        existingTopping.count += 1;
+        existingTopping.count += 1
       } else {
         console.log('New Topping', topping.name);
         // otherwise create a new entry in our acc and set it to one
@@ -46,13 +46,13 @@ function countPizzasInToppings(pizzas) {
           count: 1,
         };
       }
-      return acc;
+      return acc
     }, {});
   // sort them based on their count
   const sortedToppings = Object.values(counts).sort(
     (a, b) => b.count - a.count
   );
-  return sortedToppings;
+  return sortedToppings
 }
 
 export default function ToppingsFilter() {
@@ -79,13 +79,14 @@ export default function ToppingsFilter() {
   `);
   // Count how many pizzas are in each topping
   const toppingsWithCounts = countPizzasInToppings(pizzas.nodes);
-  console.log(toppingsWithCounts);
+  console.clear()
+  console.log(toppingsWithCounts)
   // Loop over the list of toppings and display the topping and the count of pizzas in that topping
   // Link it up.. ...  . . .
   return (
     <ToppingsStyles>
       {toppingsWithCounts.map((topping) => (
-        <Link to={`/topping/${topping.name}`} key={topping.id}>
+        <Link to={`/topping/${topping.name}`} key={topping.id}  className="topping-filter">
           <span className="name">{topping.name}</span>
           <span className="count">{topping.count}</span>
         </Link>
